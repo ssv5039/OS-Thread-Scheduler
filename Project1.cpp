6c8671172cbd11bc709d3c1d354ecf9efc021848
Shawn <ssv5039@users.noreply.github.com>
2017-09-15 12:09:33 -0400

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -5,25 +5,22 @@
 
 int main(int argc, char* argv[])
 {
-	//Written by Steven Godshall, Ben Zug, Prottay Protivash
-	//Note: The CPUs the threads end on don't matter.  Just the finishing time.
-	//If there are any issues, please bring to our attention on canvas or elsewhere.
+	//Note: The CPUs that the threads end on don't matter. Just the finishing time does.
 
 	cout << "FCFS Policy, 2 CPUs\n";
 	MyScheduler myScheduler(FCFS, 2);
-
 	myScheduler.CreateThread(1, 5, 1, 0);
 	myScheduler.CreateThread(3, 13, 2, 1);
 	myScheduler.CreateThread(0, 32, 2, 2);
 	myScheduler.CreateThread(2, 10, 2, 3);
 	myScheduler.Go();
-	/*Our output
-
+	/*Correct Output:
 	Thread ID : 0 Finishing Time : 6 CPU No. : 1
 	Thread ID : 3 Finishing Time : 16 CPU No. : 1
 	Thread ID : 1 Finishing Time : 29 CPU No. : 1
 	Thread ID : 2 Finishing Time : 32 CPU No. : 0
 	*/
+	
 	cout << "STRFwoP Policy, 3 CPUs\n";
 	MyScheduler ms2 = MyScheduler(Policy::STRFwoP, 3);
 	ms2.CreateThread(0, 12, 0, 0);
@@ -35,8 +32,7 @@ int main(int argc, char* argv[])
 	ms2.CreateThread(1, 4, 3, 6);
 
 	ms2.Go();
-
-	/*
+	/* Correct Output:
 	Thread ID : 4 Finishing Time : 5 CPU No. : 1
 	Thread ID : 6 Finishing Time : 5 CPU No. : 2
 	Thread ID : 1 Finishing Time : 7 CPU No. : 1
@@ -46,6 +42,8 @@ int main(int argc, char* argv[])
 	Thread ID : 2 Finishing Time : 14 CPU No. : 1
 	All the Threads have been executed !! .. Exiting Scheduler ..
 	*/
+	
+	
 	cout << "STRFwoP Policy, 2 CPUs\n";
 	MyScheduler myScheduler2(STRFwoP, 2);
 	myScheduler2.CreateThread(0, 5, 1, 0);
@@ -53,13 +51,14 @@ int main(int argc, char* argv[])
 	myScheduler2.CreateThread(6, 32, 2, 2);
 	myScheduler2.CreateThread(2, 10, 2, 3);
 	myScheduler2.Go();
-
-	/*Our output
+	/*Correct Output:
 	Thread ID : 0 Finishing Time : 5 CPU No. : 0
 	Thread ID : 3 Finishing Time : 12 CPU No. : 1
 	Thread ID : 1 Finishing Time : 18 CPU No. : 0
 	Thread ID : 2 Finishing Time : 44 CPU No. : 1
 	*/
+	
+	
 	cout << "STRFwP Policy, 3 CPUs\n";
 	MyScheduler myScheduler3(STRFwP, 3);
 	myScheduler3.CreateThread(0, 20, 1, 0);
@@ -70,7 +69,7 @@ int main(int argc, char* argv[])
 	myScheduler3.CreateThread(3, 4, 2, 5);
 	myScheduler3.CreateThread(3, 5, 2, 6);
 	myScheduler3.Go();
-	/*Our output
+	/*Correct Output:
 	Thread ID : 4 Finishing Time : 5 CPU No. : 0
 	Thread ID : 3 Finishing Time : 6 CPU No. : 1
 	Thread ID : 5 Finishing Time : 7 CPU No. : 2
@@ -79,6 +78,9 @@ int main(int argc, char* argv[])
 	Thread ID : 2 Finishing Time : 24 CPU No. : 2
 	Thread ID : 0 Finishing Time : 29 CPU No. : 0
 	*/
+	
+	
+	
 	cout << "STRFwP Policy, 3 CPUs\n";
 	MyScheduler ms3 = MyScheduler(Policy::STRFwP, 3);
 	ms3.CreateThread(0, 12, 0, 0);
@@ -90,8 +92,7 @@ int main(int argc, char* argv[])
 	ms3.CreateThread(1, 4, 3, 6);
 
 	ms3.Go();
-
-	/*
+	/* Correct Output:
 	Thread ID : 6 Finishing Time : 5 CPU No. : 0
 	Thread ID : 4 Finishing Time : 5 CPU No. : 1
 	Thread ID : 5 Finishing Time : 7 CPU No. : 0
@@ -102,6 +103,7 @@ int main(int argc, char* argv[])
 	All the Threads have been executed !! .. Exiting Scheduler ..
 	*/
 
+	
 	cout << "PBS Policy, 3 CPUs\n";
 	MyScheduler myScheduler4(PBS, 3);
 	myScheduler4.CreateThread(1, 202, 20, 0);
@@ -110,7 +112,7 @@ int main(int argc, char* argv[])
 	myScheduler4.CreateThread(3, 40, 5, 3);
 	myScheduler4.CreateThread(3, 40, 3, 4);
 	myScheduler4.Go();
-	/*our output
+	/*Correct Output:
 	Thread ID : 4 Finishing Time : 43 CPU No. : 0
 	Thread ID : 3 Finishing Time : 43 CPU No. : 1
 	Thread ID : 1 Finishing Time : 203 CPU No. : 2
